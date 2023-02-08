d7a172002ef598efb843b5173a5f901d9ece7988
JesusBaez02 <87582493+JesusBaez02@users.noreply.github.com>
2023-02-08 11:25:05 -0600

    Update pms.bat
    Update pms.bat

--- COE-GGA/_EXPERIMENTS/pms.bat
+++ COE-GGA/_EXPERIMENTS/pms.bat
@@ -1,19 +1,28 @@
 @echo off
-set datetime=%date:~-4,4%-%date:~-7,2%-%date:~-10,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
-set outputfile2=..\..\..\_OUTPUT\resultados_%datetime%.txt
+cls
+setlocal enabledelayedexpansion
 
 echo Ruta actual: %cd%
 for /d %%f in (..\_INPUT\pms\*) do (
 pushd %%f
-echo Procesando Carpeta: %%f
-for %%i in (*) do (
-  echo Procesando archivo: %%i
-     echo Procesando archivo: %%i >> %outputfile2%
-    ..\..\..\..\x64\Debug\COE-GGA.exe "%%i" >> %outputfile2%
-
-)
+set sub=%%f
+set sub=!sub:*\_INPUT\pms\=!
+set outputfolder=..\_OUTPUT\!sub!_
+set datetime=%date:~-4,4%-%date:~-7,2%-%date:~-10,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
+set outputfile=resultados_!datetime!.txt
+md ..\..\!outputfolder! 2>nul
+  echo Procesando Carpeta: %%f
+  for %%i in (*) do (
+    if /i not "%%i"=="list.txt" (
+     echo Procesando archivo: %%i
+     echo Procesando archivo: %%i >> ..\..\!outputfolder!\!outputfile!
+     ..\..\..\..\x64\Debug\COE-GGA.exe "%%i" >> ..\..\!outputfolder!\!outputfile!
+    )
+  )
 popd
 )
 
-echo Proceso finalizado. Ver resultados en %outputfile2%
+echo Proceso finalizado. Ver resultados en %outputfolder%
 pause
+
+
